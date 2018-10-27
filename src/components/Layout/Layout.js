4ed6e115ca772d88ed9ce5daac689b1b51f528a9
Soner CAN <soner2@gmail.com>
2018-10-27 03:27:18 -0700

    Home works

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./Layout.module.css";
 
 const Layout = props => {
   return (
-    <div className="container-fluid h-100 ">
+    <div className={`container-fluid h-100 ${styles.Layout}`}>
       <div className="row d-flex flex-column align-items-center mt-5 h-100">
         <div className="col-7 h-100 d-flex flex-column">{props.children}</div>
       </div>
--- src/components/TaskGroupItem/TaskGroupItem.js
+++ src/components/TaskGroupItem/TaskGroupItem.js
@@ -1,29 +1,24 @@
 import React from "react";
 import styles from "./TaskGroupItem.module.css";
-// import { Link } from "react-router-dom";
 
 const TaskGroupItem = ({ group, handleGroupClick }) => {
   return (
-    <React.Fragment>
-      <li
-        className={
-          `d-flex flex-column list-group-item border-left-0 border-right-0 p-3 ` +
-          styles.liItem
-        }
+    <li
+      className={`d-flex flex-column list-group-item border-left-0 border-right-0 border-radius-0 p-3 ${
+        styles.TaskGroupItem
+      }`}
+    >
+      <a
+        className={styles.groupLink}
+        href={`/group/${group.id}`}
+        onClick={e => handleGroupClick(e, group)}
       >
-        {/* <Link to={`/group/${group.id}`}>{group.title}</Link> */}
-        <a
-          className={styles.groupLink}
-          href={`/group/${group.id}`}
-          onClick={e => handleGroupClick(e, group)}
-        >
-          {group.title}
-        </a>
-        <small className={styles.completed}>
-          {group.completed} OF {group.total} TASKS COMPLETED
-        </small>
-      </li>
-    </React.Fragment>
+        {group.title}
+      </a>
+      <small className={styles.completed}>
+        {group.completed} OF {group.total} TASKS COMPLETE
+      </small>
+    </li>
   );
 };
 
